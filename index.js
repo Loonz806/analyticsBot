9748839798981fbfcf6566efe47196f82fe01792
Lenny Peters <lpeters3@lululemon.com>
2019-06-19 19:59:07 -0700

    feat(index.js): ABT-123 - analytics bot example for Slack, to listen for requests for help etc.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const SlackBots = require("slackbots");
-const axios = require("axios");
+// When you wanna add more API stuffs
+// const axios = require("axios");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -13,12 +14,63 @@ const bot = new SlackBots({
 // Start handler
 bot.on("start", () => {
   const params = {
-    icon_emoji: ":cat:"
+    icon_emoji: ":chart_with_upwards_trend:"
   };
 
   bot.postMessageToChannel(
-    "general",
-    "Hello it's me AnalyticsRequestBot",
+    "thegarage",
+    "Greetings human it's me @analyticsbot",
     params
   );
 });
+
+// Common actions
+function runHelp() {
+  const params = {
+    icon_emoji: ":question:"
+  };
+
+  bot.postMessageToChannel(
+    "thegarage",
+    `Type @analyticsbot with either 'analytics', 'digitalData', or 'dataLayer'`,
+    params
+  );
+}
+
+// Respond to data
+function handleMessage(message) {
+  const params = {
+    icon_emoji: ":chart_with_upwards_trend:"
+  };
+  if (
+    message.includes(" analytics") ||
+    message.includes(" dataLayer") ||
+    message.includes(" digitalData")
+  ) {
+    bot.postMessageToChannel(
+      "thegarage",
+      "Have you tried the request form `http://bit.ly/LuluAnalyticsRequest`",
+      params
+    );
+  } else if (message.includes(" help")) {
+    runHelp();
+  } else if (message.includes(" hello")) {
+    bot.postMessageToChannel(
+      "thegarage",
+      "Greetings data interested human. Have you tried the 'help'?",
+      params
+    );
+  }
+}
+
+// Message Handler
+bot.on("message", data => {
+  if (data.type !== "message") {
+    return;
+  }
+  handleMessage(data.text);
+});
+
+// Error Handler
+// eslint-disable-next-line no-console
+bot.on("error", err => console.log(err));
